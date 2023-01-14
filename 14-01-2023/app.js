6b88a798886dc0ae89caffe4a1577839ca4a115c
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-14 17:06:00 +0500

    14-01-2023

---
+++ 14-01-2023/app.js
@@ -0,0 +1,54 @@
+// var data=["Sameer",23,"03072200001","North Nazimabad","someone@example.com"]
+// var data=[]
+// data[2]="03316038398"
+// data[1]=25
+// data[0]="John"
+// data[4]="North Nazimabad"
+// data.shift()
+// data.unshift("John","Harry","Tom")
+// data.splice(1,1)
+// data.splice(0, 1,"duck", "emu");
+// var copyData=data.slice(2,5)
+// alert(copyData)
+// data.push("Dummy text")
+// data.pop()
+// alert(data)
+
+// var counties=[1,2,3,4,5]
+
+// counties[1]="John"
+// counties[3]="Doe"
+
+// alert(counties)
+
+// let arr= [11,26,72,35,49,6]
+
+// for(let i=0;i<arr.length;i++){   //0 1
+//    if(arr[i]%2!==0){
+//        document.write(arr[i]+"<br>")
+//    }
+// }
+
+// let arr= [1,2,3,4,5,6,7,8,9]
+// let product=1
+// for(let i=0;i<arr.length;i++){  
+//    product=product*arr[i]
+// }
+// alert(product)
+
+let arr= [1,2,3,4,5,6,7,8,9]
+let sum=0
+for(let i in arr){
+    sum=sum+arr[i]
+    
+}
+document.write("The sum of the array is "+sum+" in numbers")
+
+// let arr= [1,2,3,4,5,6,7,8,9]
+// alert(arr.length)
+
+// let str="I love my country"
+// alert(str.length)
+
+// alert(str)
+
